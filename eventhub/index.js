e081a29e190523a9b98679e1ac73cb65f929ffb5
Nirbhayadh <68394007+Nirbhayadh@users.noreply.github.com>
2021-04-27 09:00:53 +0545

    Delete index.js

--- eventhub/index.js
+++
@@ -1,8 +0,0 @@
-
-// $(".navbar-brand").hover(function() {
-//   $(".logo-img").css("width","20%");
-// }, function(){
-//   $(".logo-img").css("width","40px");
-//   $(".logo-img").css("height","32px");
-// }
-// );
