cfb05409cf3cbae1c58272656d92739761bb5ec5
Nirbhayadh <68394007+Nirbhayadh@users.noreply.github.com>
2021-05-02 22:53:16 +0545

    Add files via upload
    hover js logo, foot-icons

--- eventhub/index.js
+++ eventhub/index.js
@@ -1,8 +1,22 @@
 
-// $(".navbar-brand").hover(function() {
-//   $(".logo-img").css("width","20%");
-// }, function(){
-//   $(".logo-img").css("width","40px");
-//   $(".logo-img").css("height","32px");
-// }
-// );
+$(".navbar-brand").hover(function() {
+  $(".navbar-logo-img").css("transform","scale(1.1)");
+  $(".navbar-logo-img").css("opacity","1");
+
+}, function(){
+  $(".navbar-logo-img").css("transform","");
+  $(".navbar-logo-img").css("opacity","");
+
+}
+);
+
+
+$(".foot-icon").hover(function() {
+
+  $(this).css("opacity","1");
+
+}, function(){
+  $(".foot-icon").css("opacity","");
+
+}
+);
