e9420420d9de26cd4eafde275ace063d8735bc74
Kamal <neupane0620040@outlook.com>
2021-05-03 09:05:21 +0545

    Implemented model for user

--- users/models.py
+++ users/models.py
@@ -2,6 +2,22 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-class user(AbstractUser):
+class User(AbstractUser):
 
-    pass
+    '''Custom User model'''
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE,"Female"),
+        (GENDER_OTHER,"Other")
+    )
+
+    avatar = models.ImageField(null=True,blank = True)
+    gender = models.CharField(choices=GENDER_CHOICES,max_length=10,null=True,blank=True)
+    bio = models.TextField(default="")
+    superorganizer = models.BooleanField(default = False)
+    
