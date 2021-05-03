646d20b6b97851a15dcd2bea70f93bec8d11822e
Kamal <neupane0620040@outlook.com>
2021-05-03 16:17:42 +0545

    Implemented Conversation model

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ DJANGO_APPS = [
 ]
 
 PROJECT_APPS =[
-    "core.apps.CoreConfig","users.apps.UsersConfig","events.apps.EventsConfig","reviews.apps.ReviewsConfig","reservations.apps.ReservationsConfig","lists.apps.ListsConfig",
+    "core.apps.CoreConfig","users.apps.UsersConfig","events.apps.EventsConfig","reviews.apps.ReviewsConfig","reservations.apps.ReservationsConfig","lists.apps.ListsConfig","conversations.apps.ConversationsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
