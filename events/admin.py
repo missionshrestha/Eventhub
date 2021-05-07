215fd0b5f492bb80b801346c76f463603260f91f
Kamal <neupane0620040@outlook.com>
2021-05-07 10:56:33 +0545

    Implemented photo admin (mark_safe)

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe  #mark a string safe for HTML output porpose
 from . import models
 
 # Register your models here.
@@ -59,4 +60,10 @@ class EventAdmin(admin.ModelAdmin):
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    pass
+    
+    """Photo admin defination"""
+    list_display = ("__str__","get_thumbnail")
+
+    def get_thumbnail(self,obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+    get_thumbnail.short_description = "Thumbnails"
