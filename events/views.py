a6af52ca2af695c85227a9d852bc9bb9e4efee8c
Kamal <neupane0620040@outlook.com>
2021-05-08 11:36:26 +0545

    Created some templates

--- events/views.py
+++ events/views.py
@@ -4,4 +4,4 @@ from . import models
 # Create your views here.
 def all_events(request):
     all_events = models.Event.objects.all()
-    return render(request,"all_events.html",context={"event":all_events})
+    return render(request,"events/all_events.html",context={"event":all_events})
