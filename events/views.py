a6f39d196a0a5c1edc39e5693545d9e223d32662
Kamal <neupane0620040@outlook.com>
2021-05-08 16:22:32 +0545

    implemented pagination for event

--- events/views.py
+++ events/views.py
@@ -3,5 +3,9 @@ from . import models
 
 # Create your views here.
 def all_events(request):
-    all_events = models.Event.objects.all()
+    page = int(request.GET.get("page",1))
+    page_size = 6
+    limit = page_size * page
+    offset = limit - page_size
+    all_events = models.Event.objects.all()[offset:limit]
     return render(request,"events/all_events.html",context={"event":all_events})
