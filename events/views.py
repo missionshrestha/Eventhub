9009ea31e601f44d42939595bec841862782a2ef
Kamal <neupane0620040@outlook.com>
2021-05-11 21:10:04 +0545

    Done Implementing searching

--- events/views.py
+++ events/views.py
@@ -44,7 +44,7 @@ def search(request):
     event_type = int(request.GET.get("event_type",0))
     event_types = models.EventType.objects.all()
     price = int(request.GET.get("price",0))
-    super_organizer = request.GET.get("super_organizer",False)
+    super_organizer = bool(request.GET.get("super_organizer",False))
     choices = {
         "event_types":event_types,
     }
@@ -56,8 +56,22 @@ def search(request):
         "address":address,
         "super_organizer":super_organizer,
     }
-    
-    return render(request, "events/search.html",{**form,**choices})
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    if event_type != 0:
+        filter_args["event_type__pk__exact"] = event_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if super_organizer is True:
+        filter_args["organizer__superorganizer"] = True
+
+    events = models.Event.objects.filter(**filter_args)
+    return render(request, "events/search.html",{**form,**choices,"events":events,"price":price,"super_organizer":super_organizer,})
 
 
 
