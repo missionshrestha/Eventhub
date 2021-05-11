8300a725612556547f258ba8888fb57b95b8b9e1
Kamal <neupane0620040@outlook.com>
2021-05-11 10:29:58 +0545

    Set DEBUG = TRUE and ALLOWED_HOST =[], so now 404 will not works

--- config/settings.py
+++ config/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_moej60bf5qc=#j(s4kush&!34zu#jco&@n1@3rzx^me-yn5l#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = []
 
 
 # Application definition
