22247da9c55fd7fb492a9933e15aa9b5b6d73d6b
manidumaru <manidumaru36@gmail.com>
2021-07-24 07:16:10 +0545

    deleted

--- Manee/Eventhub-Manee/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from events import views as event_views
-from . import views
-app_name = "core"
-
-urlpatterns =[
-    path("",views.home_view,name="home"),
-    path("events/",event_views.EventView.as_view(),name="event"),
-    ]
--- Manee/Eventhub-Manee/reviews/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-@admin.register(models.Review)
-class ReviewAdmin(admin.ModelAdmin):
-    """ Review Admin Model """
-    
-    list_display = ('__str__',"rating")
